bfcb32787beb16c157594f7de24e3dbb39d27e09
kazuho <malaysia.cryer@gmail.com>
2021-05-04 23:03:03 +0900

    [added] i2c function

---
+++ ccs811.c
@@ -0,0 +1,157 @@
+#include "ccs811.h"
+
+#define ENABLE_ACK_CHECK (true)
+#define DISABLE_NACK_CHECK (false)
+
+static ccs811_config_t *config_table;
+
+/* CCS811の初期化を行う。 */
+int8_t ccs811_init(ccs811_config_t *ccs811_config)
+{
+    if (ccs811_config == NULL) {
+        return CCS811_ERROR;
+    }
+    config_table = ccs811_config;
+
+    int8_t status = CCS811_SUCCESS;
+    uint8_t data = 0;
+
+    status = ccs811_read_reg(0x20, &data, 1);
+    printf("%d\n", data);
+
+    return status;
+}
+
+/* CCS811レジスタへの書き込み */
+int8_t ccs811_write_reg(uint8_t reg_address, uint8_t *data, uint8_t size)
+{
+    if (data == NULL) {
+        return CCS811_ERROR;
+    }
+
+    int8_t status = CCS811_SUCCESS;
+
+    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
+    status = i2c_master_start(cmd_handle);
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write_byte(cmd_handle,
+                                       (CCS811_DEV_ADDR << 1) | I2C_MASTER_WRITE,
+                                       ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write_byte(cmd_handle, reg_address, ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write(cmd_handle, data, size, ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_stop(cmd_handle);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_cmd_begin(I2C_NUM_0, cmd_handle, 10 / portTICK_PERIOD_MS);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        i2c_cmd_link_delete(cmd_handle);
+    } else {
+        goto ERROR;
+    }
+
+    return status;
+
+ERROR:
+    return CCS811_ERROR;
+}
+
+/* CCS811レジスタの読み取り */
+int8_t ccs811_read_reg(uint8_t reg_address, uint8_t *data, uint8_t size)
+{
+    if (data == NULL) {
+        return CCS811_ERROR;
+    }
+
+    int8_t status = CCS811_SUCCESS;
+
+    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
+    status = i2c_master_start(cmd_handle);
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write_byte(cmd_handle,
+                                       (CCS811_DEV_ADDR << 1) | I2C_MASTER_WRITE,
+                                       ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write_byte(cmd_handle, reg_address, ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_start(cmd_handle);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_write_byte(cmd_handle,
+                                       (CCS811_DEV_ADDR << 1) | I2C_MASTER_READ,
+                                       ENABLE_ACK_CHECK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        if (size > 1) {
+            status = i2c_master_read(cmd_handle, data, size - 1, I2C_MASTER_ACK);
+        }
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_read_byte(cmd_handle, data+size-1, I2C_MASTER_ACK);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_stop(cmd_handle);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        status = i2c_master_cmd_begin(I2C_NUM_0, cmd_handle, 10 / portTICK_PERIOD_MS);
+    } else {
+        goto ERROR;
+    }
+
+    if (status == CCS811_SUCCESS) {
+        i2c_cmd_link_delete(cmd_handle);
+    } else {
+        goto ERROR;
+    }
+
+    return status;
+
+ERROR:
+    return CCS811_ERROR;
+}
+
---
+++ ccs811.h
@@ -0,0 +1,79 @@
+#ifndef __CCS811_H__
+#define __CCS811_H__
+
+#include <stdint.h>
+#include "driver/i2c.h"
+
+#define CCS811_DEV_ADDR (0x5A)
+
+enum ccs811_status {
+    CCS811_ERROR = -1,
+    CCS811_SUCCESS,
+};
+
+enum ccs811_drive_mode {
+    CCS811_DRIVE_MODE_IDLE = 0,
+    CCS811_DRIVE_MODE_CONSTANT_POWER_1000MS,
+    CCS811_DRIVE_MODE_PULSE_HEATING,
+    CCS811_DRIVE_MODE_LOW_POWER_PULSE_HEATING,
+    CCS811_DRIVE_MODE_CONSTANT_POWER_250MS,
+};
+
+/* 測定完了割り込みの設定。
+ * ENABLEとした場合、測定完了ごとに[nINT]がlowに駆動される。
+ * ALG_RESULT_DATAをreadすることで自動的に[nINT]のlow駆動は解除される。 */
+enum ccs811_interrupt_enable {
+    CCS811_INTERRUPT_DISABLE = 0,
+    CCS811_INTERRUPT_ENABLE,
+};
+
+/* eCO2絶対値による割り込みの設定。
+ * ENABLEとした場合、測定値がTHRESHOLDSレジスタに設定された値を上回ると[nINT]がlowに駆動される。
+ * ALG_RESULT_DATAをreadすることで自動的に[nINT]のlow駆動は解除される。 */
+enum ccs811_thresh_enable
+{
+    CCS811_THRESH_ENABLE = 0,
+    CCS811_THRESH_DISABLE,
+};
+
+typedef struct ccs811_config {
+    uint8_t drive_mode;
+    uint8_t interruput_enable;
+    uint8_t thresh_enable;
+} ccs811_config_t;
+
+typedef struct ccs811_measure_data {
+    uint16_t eco2;
+    uint16_t tvoc;
+    uint8_t status;
+    uint8_t error_id;
+    uint16_t raw_data;
+} ccs811_measure_data_t;
+
+typedef struct ccs811_environment_data {
+    int32_t temperature;
+    uint32_t humidity;
+} ccs811_environment_data_t;
+
+typedef struct ccs811_eco2_threshold {
+    uint16_t upper_thresh;
+    uint16_t lower_thresh;
+} ccs811_eco2_threshold_t;
+
+int8_t ccs811_init(ccs811_config_t *ccs811_config);
+int8_t ccs811_exit();
+int8_t ccs811_one_shot_measure(ccs811_measure_data_t *measure_data);
+int8_t ccs811_get_measure_data(ccs811_measure_data_t *measure_data);
+int8_t ccs811_write_environment_value(ccs811_environment_data_t *environment_data);
+int8_t ccs811_write_eco2_threshold(ccs811_eco2_threshold_t *eco2_threshold);
+int8_t ccs811_get_hardware_id(uint8_t *hardware_id);
+int8_t ccs811_get_hardware_version(uint8_t *hardware_version);
+int8_t ccs811_get_firmware_version(uint16_t *firmware_version);
+int8_t ccs811_get_firmware_app_version(uint16_t *firmware_app_version);
+int8_t ccs811_get_error_id(uint8_t *error_id);
+int8_t ccs811_app_verify();
+int8_t ccs811_software_reset();
+int8_t ccs811_read_reg(uint8_t reg_addr, uint8_t *data, uint8_t size);
+int8_t ccs811_write_reg(uint8_t reg_addr, uint8_t *data, uint8_t size);
+
+#endif /* __CCS811_H__ */
